a690cf98eaaa51555a017a3df43184ca6dea3690
Nikita Tikhonov <jagarnikita@gmail.com>
2024-06-15 13:49:54 +0200

    Add test SwiftUI view

---
+++ ios/RandomText.swift
@@ -0,0 +1,18 @@
+//
+//  RandomText.swift
+//  newarchswiftui
+//
+//  Created by Nikita Tikhonov on 15.06.2024.
+//
+
+import SwiftUI
+
+struct RandomText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomText()
+}
---
+++ ios/RandomTextGetter.swift
@@ -0,0 +1,8 @@
+//
+//  RandomTextGetter.swift
+//  newarchswiftui
+//
+//  Created by Nikita Tikhonov on 15.06.2024.
+//
+
+import Foundation
